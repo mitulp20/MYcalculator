e4b6b4d02fcba49795f0d84ed44d2a05a0c0da33
Mitul Patel <mitulpatel@Mitul-Patels-MacBook-Pro.local>
2013-07-27 11:28:09 -0400

    Firstcomment

--- Mycalculator/ViewController.h
+++ Mycalculator/ViewController.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    float result;
+    IBOutlet UILabel *Mycalculatorscreen;
+    IBOutlet UIImage *imageview;
+    int currentOperation;
+    float currentNumber;
+    BOOL userInMiddleOfTypingANumber;
+    
+}
+
+-(IBAction)buttonDigitPressed: (id)sender;
+-(IBAction)buttonOperationPressed: (id)sender;
+-(IBAction)cancelInput;
+-(IBAction)cancelOperation;
+-(IBAction)decimalPressed:(id)sender;
 
 @end
--- Mycalculator/ViewController.m
+++ Mycalculator/ViewController.m
@@ -14,6 +14,108 @@
 
 @implementation ViewController
 
+-(IBAction)decimalPressed:(id)sender{
+    NSString *currentDigits = Mycalculatorscreen.text;
+    Mycalculatorscreen.text = [currentDigits stringByAppendingString:@"."];
+}
+
+- (IBAction)buttonDigitPressed:(id)sender {
+    if(userInMiddleOfTypingANumber == YES)
+    {
+        
+        NSRange range = [Mycalculatorscreen.text rangeOfString:@"."];
+            if([[[sender titleLabel]text] isEqual:@"."])
+        
+        {
+            if (!(range.location ==NSNotFound)){
+                
+                }
+                else
+                {
+                    NSString *currentDigits = Mycalculatorscreen.text;
+                    Mycalculatorscreen.text = [currentDigits stringByAppendingString:@"."];}
+            }
+        else{
+     NSString *currentDigits = Mycalculatorscreen.text;
+    NSString *newDigit = [[sender titleLabel]text];
+    Mycalculatorscreen.text=[currentDigits stringByAppendingString:newDigit];
+    currentNumber=[Mycalculatorscreen.text floatValue];
+         
+        }}
+    
+    else
+    {
+        if([[[sender titleLabel]text] isEqual:@"."])
+            
+        {
+            NSString *currentDigits = @"";
+            Mycalculatorscreen.text = [currentDigits stringByAppendingString:@"."];
+        }
+        else{
+            NSString *currentDigits = @"";
+            NSString *newDigit = [[sender titleLabel]text];
+            Mycalculatorscreen.text=[currentDigits stringByAppendingString:newDigit];
+            currentNumber=[Mycalculatorscreen.text floatValue];
+            
+        }
+        userInMiddleOfTypingANumber=YES;
+    
+    }
+        }
+    
+//    currentNumber = currentNumber *10 + (float)[sender tag];
+//    Mycalculatorscreen.text = [ NSString stringWithFormat:@"%2g" ,currentNumber];
+    
+
+
+- (IBAction)buttonOperationPressed:(id)sender {
+    if (currentOperation == 0) result = currentNumber;
+    else {
+        switch (currentOperation) {
+            case 1:
+                result = result + currentNumber;
+                userInMiddleOfTypingANumber = NO;
+                break;
+            case 2:
+                result = result - currentNumber;
+                userInMiddleOfTypingANumber = NO;
+                break;
+            case 3:
+                result = result * currentNumber;
+                userInMiddleOfTypingANumber = NO;
+                break;
+            case 4:
+                result = result / currentNumber;
+                userInMiddleOfTypingANumber = NO;
+                break;
+            case 5:
+                currentOperation = 0;
+                
+                break;
+            
+        
+        }
+    }
+    currentNumber = 0;
+    Mycalculatorscreen.text = [NSString stringWithFormat:@"%2g" ,result];
+    if ( [sender tag] == 0) result =0;
+    currentOperation = [sender tag ];
+    userInMiddleOfTypingANumber=NO;
+         
+    
+}
+
+-(IBAction)cancelInput {
+    currentNumber = 0;
+    Mycalculatorscreen.text = @"0";
+}
+
+- (IBAction) cancelOperation {
+    currentNumber = 0;
+    Mycalculatorscreen.text = @"0";
+    currentOperation = 0;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
